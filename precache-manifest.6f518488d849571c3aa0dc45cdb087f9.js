152bfb95c414c389344609b65371282f97fe5be6
Wayne Cai <cjqcraft@gmail.com>
2019-02-16 15:23:27 -0400

    Updates

---
+++ precache-manifest.6f518488d849571c3aa0dc45cdb087f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/foodradar/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/foodradar/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "eaf5f4302dcd17ec5adc",
+    "url": "/foodradar/static/js/runtime~main.eaf5f430.js"
+  },
+  {
+    "revision": "63373e1ba0a1ea80bad7",
+    "url": "/foodradar/static/js/main.63373e1b.chunk.js"
+  },
+  {
+    "revision": "e330cb724c0a43195191",
+    "url": "/foodradar/static/js/2.e330cb72.chunk.js"
+  },
+  {
+    "revision": "63373e1ba0a1ea80bad7",
+    "url": "/foodradar/static/css/main.9611ace6.chunk.css"
+  },
+  {
+    "revision": "b63137fa6654bb77b0452a499cc4423a",
+    "url": "/foodradar/index.html"
+  }
+];
